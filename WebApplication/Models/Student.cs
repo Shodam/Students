5a8def4b21574d4d56d50a8122ee5e8ba13d242f
Alberto Juan Canete <Acane017@fiu.edu>
2021-08-11 01:21:02 -0400

    Changed Titles, added a few more texts in the website. Future Implementation: An admin account with user name and password to delete or edit student details.

--- WebApplication/Models/Student.cs
+++ WebApplication/Models/Student.cs
@@ -10,20 +10,12 @@ namespace WebApplication.Models
 {
     public class Student
     {
-        [Required(ErrorMessage = "Student Id is required.")]
-        [Column("StudentID")]
         public int StudentId { get; set; }
         
-        [Required(ErrorMessage = "First name is required.")]
-        [Column("FirstName")]
         public string FirstName { get; set; }
         
-        [Required(ErrorMessage = "Last name is required.")] 
-        [Column("LastName")]
         public string LastName { get; set; }
         
-        [Required(ErrorMessage = "GPA is required.")] 
-        [Column("GPA")]
         public double Gpa { get; set; }
     }
 }
