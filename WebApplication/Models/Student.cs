6c0d467d17fbf051e993ba823ed00b841b6f2adc
Alberto Juan Canete <Acane017@fiu.edu>
2021-08-10 22:56:26 -0400

    Changed the model architecture and controller for student database

--- WebApplication/Models/Student.cs
+++ WebApplication/Models/Student.cs
@@ -1,11 +1,25 @@
+using System.Data;
+using System.Threading.Tasks;
+using System;    
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using MySql.Data.MySqlClient;
 namespace WebApplication.Models
 {
     public class Student
     {
-        private StudentContext context;
+        [Required(ErrorMessage = "First name is required.")] 
         public int StudentId { get; set; }
+        
+        [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Last name is required.")] 
         public string LastName { get; set; }
+        
+        [Required(ErrorMessage = "GPA is required.")] 
         public double Gpa { get; set; }
     }
 }
